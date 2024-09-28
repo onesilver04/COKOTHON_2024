ada94b57d1726c7121f1ae21de2d81041bb810c2
YimTaekeun <ytk030305@gmail.com>
2024-09-28 22:46:37 +0900

    [FEAT]: made Header

--- src/pages/Test/testPage.js
+++ src/pages/Test/testPage.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Footer from "../../components/Footer";
+import Header from "../../components/Header";
 
 const TestPage = () => {
   return (
     <div>
-      <Footer />
+      <Header title="제목" />
     </div>
   );
 };
