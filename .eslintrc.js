513dca5e4f4bfbe12be66f212da085e2215ad540
hyeonjin6530 <cola1876@naver.com>
2024-09-28 21:26:23 +0900

    Fix: eslint 에러 -> 경고 발생

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,31 +25,31 @@ module.exports = {
     // JSX에 React가 필요하지 않으므로 비활성화
     "react/react-in-jsx-scope": "off",
     // Stateless 함수형 컴포넌트를 권장하지 않으므로 비활성화
-    "react/prefer-stateless-function": "off",
+    "react/prefer-stateless-function": "warn",
     // JSX 파일 확장자를 .js, .jsx, .ts, .tsx로 허용
     "react/jsx-filename-extension": [
-      "error",
+      "warn",
       { extensions: [".js", ".jsx", ".ts", ".tsx"] },
     ],
     // JSX에서 한 줄에 하나의 표현식을 강제하지 않음
-    "react/jsx-one-expression-per-line": "off",
+    "react/jsx-one-expression-per-line": "warn",
     // 중첩된 삼항 연산자를 사용해도 오류가 발생하지 않음
-    "no-nested-ternary": "off",
+    "no-nested-ternary": "warn",
     // PropTypes 검사를 비활성화
     "react/prop-types": "off",
     // 의존성 파일 검사 비활성화
-    "import/no-extraneous-dependencies": "off",
+    "import/no-extraneous-dependencies": "warn",
     // 화살표 함수의 본문을 항상 중괄호로 감싸야 함
-    "arrow-body-style": ["error", "always"],
+    "arrow-body-style": ["warn", "always"],
     // 함수형 컴포넌트 정의를 화살표 함수로 강제하지 않음
     "react/function-component-definition": [
-      "off",
+      "warn",
       {
         namedComponents: "arrow-function", // 이름이 있는 컴포넌트는 화살표 함수로 정의
         unnamedComponents: "arrow-function", // 이름이 없는 컴포넌트도 화살표 함수로 정의
       },
     ],
-    // 문자열에 큰따옴표 사용을 강제
-    quotes: ["error", "double"],
+    // 문자열에 큰따옴표 사용을 경고
+    quotes: ["warn", "double"],
   },
 };
