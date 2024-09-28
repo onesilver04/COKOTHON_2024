3fecaa00c81c8c234e28c0904d1300c322d0836b
hyeonjin6530 <cola1876@naver.com>
2024-09-29 08:49:09 +0900

    Fix: 에러 수정

--- src/pages/Test/Mypage/Mypage.js
+++ src/pages/Test/Mypage/Mypage.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios"; // axios 임포트
 import { Link } from "react-router-dom";
 import "./Mypage.css";
+import axios from "axios"; // axios 임포트
 import Note from "../../../components/Note"; // Note 컴포넌트 임포트
+import dummy from "../dummy"; // dummy 데이터를 임포트
 import Footer from "../../../components/Footer";
 import Header from "../../../components/Header";
 
@@ -30,45 +31,16 @@ const Mypage = () => {
     }
   };
 
+  // 컴포넌트가 마운트될 때 유저 정보 가져오기
   useEffect(() => {
-    getInfo(); // 컴포넌트가 마운트될 때 유저 정보 가져오기
-  }, []);
+    getInfo();
+  }, []); // 빈 배열을 전달하여 한 번만 호출되도록 설정
 
+  // 로딩 중 상태 처리
   if (!userInfo) {
     return <div>Loading...</div>; // 유저 정보가 로딩 중일 때 표시할 내용
   }
 
-  const [notes, setNotes] = useState([]);
-  useEffect(() => {
-    // 비동기 데이터를 가져오는 함수를 정의
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/v1/wrong-notes`, {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-            Accept: "application/json",
-          },
-          params: {
-            count: 5,
-          },
-          withCredentials: true,
-        });
-        console.log(response.data);
-        setNotes(response.data);
-        // 필요한 경우, 상태 업데이트 등을 여기서 처리
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData(); // 정의한 비동기 함수를 호출
-
-    // 클린업 함수 반환 (필요한 경우에만)
-    return () => {
-      // 컴포넌트가 언마운트될 때 실행되는 정리 작업
-      console.log("RankPage component unmounting...");
-    };
-  }, []);
   return (
     <>
       <Header title="My Page" />
@@ -78,13 +50,11 @@ const Mypage = () => {
           <h2 className="profile-name">{userInfo.nickname}</h2>
           <p className="profile-info">
             내 마일리지 |
-            {" "}
             {userInfo.point}
             점
           </p>
           <p className="profile-info">
             내 레벨 |
-            {" "}
             {Math.floor(userInfo.xp / 10)}
             {" "}
             Lv
@@ -103,13 +73,13 @@ const Mypage = () => {
             </Link>
           </div>
 
-          {notes.map((item) => {
+          {dummy.map((item) => {
             return (
               <Note
-                key={item.quizId}
-                wrong={item.answerIndex === 1 ? item.options[0] : item.options[1]}
-                right={item.answerIndex === 1 ? item.options[1] : item.options[0]}
-                define={item.question}
+                key={item.id}
+                wrong={item.wrong}
+                right={item.right}
+                define={item.define}
               />
             );
           })}
